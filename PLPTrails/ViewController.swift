cbc8d41bcf5fd82f8ac2470878b8600b194e9ea6
Matthew Beatty <beattymg@gmail.com>
2015-12-30 13:53:22 -0500

    Initial Commit

---
+++ PLPTrails/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PLPTrails
+//
+//  Created by Matthew Beatty on 12/30/15.
+//  Copyright (c) 2015 Matthew Beatty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
