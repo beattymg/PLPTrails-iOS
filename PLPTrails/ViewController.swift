27677b461b65fd32873698fc0e73e0cd575dbf31
Matthew Beatty <beattymg@gmail.com>
2015-12-31 11:58:00 -0500

    initial commit

--- PLPTrails/ViewController.swift
+++ PLPTrails/ViewController.swift
@@ -7,19 +7,23 @@
 //
 
 import UIKit
+import Mapbox
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // fill in the next line with your style URL from Mapbox Studio
+        let styleURL = NSURL(string: "mapbox://styles/beattymg/ciiug67uw00izzwmag6e6l10d")
+        let mapView = MGLMapView(frame: view.bounds,
+            styleURL: styleURL)
+        mapView.autoresizingMask = .FlexibleWidth | .FlexibleHeight
+        
+        // set the map's center coordinate
+        mapView.setCenterCoordinate(CLLocationCoordinate2D(latitude: 41.1,
+            longitude: -75.530),
+            zoomLevel: 12, animated: false)
+        view.addSubview(mapView)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
---
+++ README.md
@@ -0,0 +1,1 @@
+# PLPTrails-iOS
